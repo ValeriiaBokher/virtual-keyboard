f2ec23e9717c5a339e412ce72504cf1188893839
Valeriia Bokher <valeriiabokher@gmaol.com>
2023-04-28 16:45:40 +0300

    feat: keyboard and mousr type in textarea

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ textArea.after(keyboard);
 
 // BUTTONS
 var button = '';
-const buttons = document.querySelectorAll('.buttons');
 import letters from './letters.js';
 function fill() {
 	for (let i = 0; i < letters.length; i++) {
@@ -35,15 +34,49 @@ function fill() {
 }
 fill();
 
+// ввод с помощью мыши
+const buttons = document.querySelectorAll('.buttons');
+for (let i = 0; i < buttons.length; i++) {
+	buttons[i].addEventListener('click', function (e) {
+		let _this = this;
+		let attribute = _this.getAttribute('data');
+		textArea.append(letters[attribute].eng[0]);
+		console.log(attribute);
+	});
+}
+
 // при нажатии кнопки на клавиатуре включается анимания
 document.addEventListener('keydown', function (event) {
 	console.log(event);
 	for (let i = 0; i < letters.length; i++) {
-		for (let j = 0; j < letters[i].all.length; j++) {
-			if (event.key == letters[i].all[j]) {
+		if (event.key == 'Shift') {
+			if (event.code == letters[i].code) {
 				document
 					.querySelector('.buttons[data="' + letters[i].button + '"]')
 					.classList.add('_active');
+			}
+		} else if (event.key == 'Control') {
+			if (event.code == letters[i].code) {
+				document
+					.querySelector('.buttons[data="' + letters[i].button + '"]')
+					.classList.add('_active');
+			}
+		} else if (event.key == 'Alt') {
+			if (event.code == letters[i].code) {
+				document
+					.querySelector('.buttons[data="' + letters[i].button + '"]')
+					.classList.add('_active');
+			}
+		} else {
+			for (let j = 0; j < letters[i].all.length; j++) {
+				if (event.key == letters[i].all[j]) {
+					let divButton = document.querySelector(
+						'.buttons[data="' + letters[i].button + '"]'
+					);
+
+					divButton.classList.add('_active');
+					divButton.click();
+				}
 			}
 		}
 	}
@@ -53,12 +86,41 @@ document.addEventListener('keydown', function (event) {
 document.addEventListener('keyup', function (event) {
 	3;
 	for (let i = 0; i < letters.length; i++) {
-		for (let j = 0; j < letters[i].all.length; j++) {
-			if (event.key == letters[i].all[j]) {
-				document.querySelectorAll('.buttons').forEach(function (elem) {
-					elem.classList.remove('_active');
-				});
-				2;
+		for (let i = 0; i < letters.length; i++) {
+			if (event.key == 'Shift') {
+				if (event.code == letters[i].code) {
+					document
+						.querySelector(
+							'.buttons[data="' + letters[i].button + '"]'
+						)
+						.classList.remove('_active');
+				}
+			} else if (event.key == 'Control') {
+				if (event.code == letters[i].code) {
+					document
+						.querySelector(
+							'.buttons[data="' + letters[i].button + '"]'
+						)
+						.classList.remove('_active');
+				}
+			} else if (event.key == 'Alt') {
+				if (event.code == letters[i].code) {
+					document
+						.querySelector(
+							'.buttons[data="' + letters[i].button + '"]'
+						)
+						.classList.remove('_active');
+				}
+			} else {
+				for (let j = 0; j < letters[i].all.length; j++) {
+					if (event.key == letters[i].all[j]) {
+						document
+							.querySelector(
+								'.buttons[data="' + letters[i].button + '"]'
+							)
+							.classList.remove('_active');
+					}
+				}
 			}
 		}
 	}
@@ -79,3 +141,14 @@ document.addEventListener('keyup', function (event) {
 // 		}
 // 	}
 // };
+
+// for (let i = 0; i < letters.length; i++) {
+// 	for (let j = 0; j < letters[i].all.length; j++) {
+// 		if (event.key == letters[i].all[j]) {
+// 			document.querySelectorAll('.buttons').forEach(function (elem) {
+// 				elem.classList.remove('_active');
+// 			});
+// 			2;
+// 		}
+// 	}
+// }
